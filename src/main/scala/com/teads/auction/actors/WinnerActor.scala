bf947a2eddf82d08ac8f7d421be9ad33356563ae
Sabine Collé-Balp <Sabine@imac-de-user-1.home>
2018-05-06 17:54:08 +0200

    that-project

--- src/main/scala/com/teads/auction/actors/WinnerActor.scala
+++ src/main/scala/com/teads/auction/actors/WinnerActor.scala
@@ -1,12 +1,14 @@
 package com.teads.auction.actors
 
 import akka.actor.{Actor, Props}
-import com.teads.auction.actors.LotteryActor.LotteryBids
 import com.teads.auction.actors.WinnerActor.LotteryComputing
+
+import scala.collection.mutable
+import scala.collection.mutable.Map
 
 object WinnerActor {
 
-  case class LotteryComputing(bids: List[LotteryBids], solution: List[Int])
+  case class LotteryComputing(bids: Map[String, List[Int]], solution: List[Int])
 
   val winnerActor = Props[WinnerActor]
 }
@@ -16,25 +18,29 @@ object WinnerActor {
   **/
 class WinnerActor extends Actor {
 
-  var mappingBid: Map[String, List[Int]] = Map.empty
+  var mappingBid: mutable.Map[String, List[Int]] = mutable.Map.empty
 
-  private def bidMapping(mapping: List[LotteryBids], maxBid: Int): List[List[Int]] = {
+  private def bidMapping(lotteryList: mutable.Map[String, List[Int]]): mutable.Map[Int, Set[String]] = {
     var matrix = List.empty[List[Int]]
-    var mapping = Map.empty[Int, List[String]]
-
-    for ((_, numbers) <- mapping) {
-      var initialize = List.fill[Int](maxBid + 1)(0)
-      println("init == " + initialize)
-      matrix = initialize :: matrix
+    var mapping = Map.empty[Int, Set[String]]
+    mapping.withDefaultValue(Set.empty[String])
+    // long life to functional.
+    for ( (name, numbers) <- lotteryList) {
+      numbers.foreach(number => {
+        mapping.get(number) match{
+          case Some(names : Set[String]) => mapping(number) =  names + name
+          case None => mapping(number) = Set(name)
+        }
+      })
     }
-    matrix
+    mapping
   }
 
   override def receive: Receive = {
 
     case lottery: LotteryComputing =>
       // O(nln(n))
-      var adjencyMatric = bidMapping(lottery.bids, lottery.solution.max)
+      var mappingLottery = bidMapping(lottery.bids)
 
   }
 }
--- src/main/scala/com/teads/auction/util/LotteryAlgorithm.scala
+++ src/main/scala/com/teads/auction/util/LotteryAlgorithm.scala
@@ -1,9 +1,45 @@
 package com.teads.auction.util
+import scala.collection.mutable
+
+
 
 object LotteryAlgorithm {
 
 
-  def cover(adjacency: List[List[Int]]): Unit = {
+  private def cover(solution: Map[Int, List[String]], input : Map[String, List[Int]], result : List[String]): List[String] = {
+    if (input.size < 1)
+      return result
+
+
+    else {
+      var tmp = List.empty[String]
+      val c = solution.keySet.min(Ordering.by( key => solution(key).size))
+      for(x <- solution(c)){
+        tmp = x :: tmp
+        solution
+      }
+    }
+  null
+  }
+
+
+  private def select(solution : Map[Int, List[String]], input : Map[String, List[Int]], r : Int): Unit ={
+    var cols = List.empty[Int]
+    for (j <- input(r)) {
+      for (i <- solution(j)){
+        for (k <- input(i)){
+
+          if (k != j){
+            solution(k) = solution(k).drop(k)
+          }
+        }
+      }
+      cols += solution(j).head
+    }
+    return cols
 
   }
+
+
+
 }
